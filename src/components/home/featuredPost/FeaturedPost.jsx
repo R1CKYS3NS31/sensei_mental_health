5cfa021e4fe1cb7191b11c828c2e4fdf73464d4a
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-11-02 13:48:35 +0300

    get help application /src... commit

--- src/components/home/featuredPost/FeaturedPost.jsx
+++ src/components/home/featuredPost/FeaturedPost.jsx
@@ -14,6 +14,7 @@ import {
   styled,
 } from "@mui/material";
 import { ExpandMoreOutlined, PsychologyAlt } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -51,10 +52,9 @@ function FeaturedPost(props) {
             <Typography variant="subtitle1" color="primary">
               <CardActions disableSpacing>
                 <IconButton aria-label="get help">
-                  Get help...
+                  <Link to={"/gethelp"} className={'link'}>Get help...</Link>
                   <PsychologyAlt />
                 </IconButton>
-             
 
                 <ExpandMore
                   expand={expanded}
