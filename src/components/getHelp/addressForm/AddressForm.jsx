9ce5506f88ec34120e36b830f604fc587303951a
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-11-06 14:46:41 +0300

    /addressForm form data  commit

--- src/components/getHelp/addressForm/AddressForm.jsx
+++ src/components/getHelp/addressForm/AddressForm.jsx
@@ -4,110 +4,148 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import { Button } from "@mui/material";
 
 export default function AddressForm() {
+  const onSubmitAddress = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      firstname: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      phoneNumber: data.get("phoneNumber"),
+      city: data.get("city"),
+      province: data.get("province"),
+      postalCode: data.get("postalCode"),
+      country: data.get("country"),
+    });
+  };
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Personal address
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-          
-            required
-            id="firstName"
-            name="firstName"
-            label="First Name"
+      <form onSubmit={onSubmitAddress}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              fullWidth
+              autoComplete="given-name"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              fullWidth
+              autoComplete="family-name"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Reachable Email"
+              fullWidth
+              autoComplete="shipping address-line1"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="phoneNumber"
+              name="phoneNumber"
+              label="phoneNumber"
+              fullWidth
+              autoComplete="shipping address-line2"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="city"
+              name="city"
+              label="City"
+              fullWidth
+              autoComplete="shipping address-level2"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="province"
+              name="province"
+              label="State/Province/Region"
+              fullWidth
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="postalCode"
+              name="postalCode"
+              label="Zip / Postal code"
+              fullWidth
+              autoComplete="shipping postal-code"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="country"
+              name="country"
+              label="Country"
+              fullWidth
+              autoComplete="shipping country"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox color="secondary" name="saveAddress" value="yes" />
+              }
+              label="Use this address for application details"
+            />
+          </Grid>
+          <Button
+            type="submit"
             fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
+            variant="contained"
+            sx={{ mt: 3, mb: 2, ml: 3 }}
+          >
+            Submit Address Details
+          </Button>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last Name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="email"
-            name="email"
-            label="Reachable Email"
-            fullWidth
-            autoComplete="shipping address-line1"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="phoneNumber"
-            name="phoneNumber"
-            label="phoneNumber"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox color="secondary" name="saveAddress" value="yes" />
-            }
-            label="Use this address for application details"
-          />
-        </Grid>
-      </Grid>
+        {/* <input
+          style={{
+            width: "100%",
+            padding: "5px",
+            fontSize: "20px",
+            fontWeight: 500,
+            backgroundColor: "lightgreen",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          type="submit"
+          value={"Submit Address Details"}
+        /> */}
+      </form>
     </React.Fragment>
   );
 }
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -30,13 +30,14 @@ function Copyright(props) {
   const theme = createTheme();
   
   export default function SignUp() {
+
     const handleSubmit = (event) => {
       event.preventDefault();
       const data = new FormData(event.currentTarget);
       console.log({
         email: data.get('email'),
         password: data.get('password'),
-        username:data.get('firstName')
+        firstname:data.get('firstName')
       });
     };
   
@@ -58,7 +59,8 @@ function Copyright(props) {
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <form onSubmit={handleSubmit}>
+            {/* <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}> */}
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
                   <TextField
@@ -124,7 +126,9 @@ function Copyright(props) {
                   </Link>
                 </Grid>
               </Grid>
-            </Box>
+            {/* </Box> */}
+            </form>
+            
           </Box>
           <Copyright sx={{ mt: 5 }} />
         </Container>
